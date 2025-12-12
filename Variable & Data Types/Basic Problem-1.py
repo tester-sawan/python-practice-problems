612878eb407e82f66a9c6a9f896a64b69b0ed115
Sawan Kumar <sawankumar.qa@gmail.com>
2025-12-12 13:07:17 +0530

    basic-1

---
+++ Variable & Data Types/Basic Problem-1.py
@@ -0,0 +1,16 @@
+"""
+Q1. Create variables to store:
+Your name
+Your age
+Your height
+Whether you are a student (True/False)
+Print all values with their data types.
+"""
+name = "Sawan Kumar"
+age = 29
+height = "5'6"
+student = True
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Is Student:",student)
